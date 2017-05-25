e78192bdf5ad62c2d26bf6ccbcb03ea8a8ef0748
Frank Iriarte <jurrish@gmail.com>
2017-05-25 14:53:08 -0700

    still scaffolding

---
+++ app/entry.js
@@ -0,0 +1,6 @@
+'use strict';
+
+require('./scss/base.scss');
+const angular = require('angular');
+
+angular.module('adventureApp', []);
---
+++ app/service/map-service.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const angular = require('angular');
+
+angular.module('adventure-app')
+.service('mapService', () => {
+  let map = {};
+
+  map.
+})
